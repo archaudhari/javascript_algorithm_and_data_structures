cf08bd9c5ab894d3a032a6ec8fc39f6372c29f48
archaudhari <81674825+archaudhari@users.noreply.github.com>
2022-02-02 23:08:15 +0530

    Ajay

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log('Hi there!')
+console.log('Hi there!') //Prints: Hi there!
+//Methods
+Math.random(); //Returns number between 0 and 1
+//Libraries
+Math.random(); //Math is the library
+//Numbers
+let amount = 6;
+let price = 4.99;
+//String.length
+let message = 'good night';
+console.log(message.length); //Prints : 9
+console.log('howdy'.length); //Prints : 5
+//Data Intances > Booleans : Booleans are a primitive data type. 
+//They can be either true or false.
+let lateToWork = true;
+//Math.random()
+console.log(Math.random()); //Prints : 0 - 0.9
+//Math.floor()
+console.log(Math.floor(5.95)); //Prints : 5
+
+//This line is single line comment
+//Null : Null is a primitive data type. 
+//It represents the intentional absence of value.
+//let x = Null;
+//Strings
+let single = 'Where my bandit hat?';
+let double = 'Wheres my bandit hat?';
+// Arithmetic Operators
+
+// JavaScript supports arithmetic operators for:
+
+// + addition
+// - subtraction
+// * multiplication
+// / division
+// % modulo
+
+//Multi-line Comments /* The below configuration must be changed before deployment*/
+let baseUrl = 'localhost/taxwebapp/country';
+//Remainder/Modulo Operator
+const weeksInYear = Math.floor(365/7); //Calculate no. of weeks
+const daysLeftOver = 365 % 7; //Calculate no. of days
+console.log("A year has " + weeksInYear + " weeks and " + daysLeftOver + " days");
