c8fc89c82f545b43d360515be126f4dea66046b8
archaudhari <81674825+archaudhari@users.noreply.github.com>
2022-02-13 23:45:43 +0530

    archaudhari

--- index.js
+++ index.js
@@ -87,3 +87,17 @@ const found = false;
 var age1 = 3;
 console.log(name1, found, age1);
 
+// Declaring Variables
+// To declare a variable in JavaScript, any of these three keywords can be used along with a variable name:
+
+// var is used in pre-ES6 versions of JavaScript.
+// let is the preferred way to declare a variable when it can be reassigned.
+// const is the preferred way to declare a variable with a constant value.
+var age;
+let weight;
+const numberOfFingers = 20;
+
+//Template literals are strings that allow embedded expressions, ${expression}.
+// While regular strings use single ' or double " quotes, template literals use backticks instead.
+let name = "Codecademy";
+console.log(`Hello, ${name}`);
